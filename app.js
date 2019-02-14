db753c5214e718f132f2513b393112daaf6fc929
WVC <wendy.canepi@ge.com>
2019-02-14 12:14:45 -0500

    Getting it started

---
+++ app.js
@@ -0,0 +1,18 @@
+
+//Declare the array
+const patientList = [
+    'Paul',
+    'Jan',
+    'Tri',
+    'Maryam'
+  ];
+  
+  alert(patientList);
+
+  //Create a function to loop
+  function loopFunc() {
+  
+    //Populate the command prompt variables
+  const command = prompt('Choose one: update, delete, add, reorder');
+  
+  }
